49d394680559963fa2c756834e5002961f16585c
Nhib1910117 <nhib1910117@student.ctu.edu.vn>
2023-02-22 15:38:35 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,11 @@
 const config = {
     app: {
-        port: process.env.PORT || 3000,
-    }
+        port: process.env.PORT || 3005,
+    },
 
-};
-module.exports = config;
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
+    }
+}
+// module.exports = config;
+module.exports = config
